f58e5d304ea80f818b71ff3b2c94df1dda540bad
delanoquirino <delanoquirino@gmail.com>
2022-11-08 20:04:15 -0300

    sociallinks edit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,25 +5,25 @@ import {MdOutlineKeyboardArrowRight} from 'react-icons/md'
 
 function Home() {
   return (
-    <div name="home" className="h-screen w-full bg-gradient-to-b from-black to-gray-800" >
+    <div name="home" className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800" >
         <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row" >
             <div className="flex flex-col justify-center h-full">
-                <h2>
-                Olá! Me chame Delano </h2>
-                <p>Sou desenvolvedor front-end e este é o meu site. Aqui você conhecerá minha jornada e projetos como desenvolvedor front-end.</p>
+                <h2 className="text-4xl sm:text-7xl font-bold text-white">
+                Olá! Me chamo Delano </h2>
+                <p className="text-gray-500 py-4 max-w-md " >Sou desenvolvedor front-end e este é o meu site. Aqui você conhecerá minha jornada e projetos como desenvolvedor.</p>
 
                 <div>
-                    <button>
+                    <button className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
                         Meus Projetos 
-                        <span>
-                        <MdOutlineKeyboardArrowRight />
+                        <span className="group-hover:rotate-90 duration-300" >
+                        <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
                         </span>
                     </button>
                 </div>
                 
             </div>
             <div>
-                    <img src={MyFoto} alt="minha foto" className="rounded-2x1 mx-auto w2/3 md:w-full " />
+                    <img src={MyFoto} alt="minha foto" className="rounded-2xl mx-auto w-2/3 md:w-full " />
             </div>
         </div>
     </div>

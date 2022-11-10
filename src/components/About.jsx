4ca88593c9dc6ab5e6ab97ee2357fc3a882a5629
delanoquirino <delanoquirino@gmail.com>
2022-11-09 21:09:32 -0300

    editando o text no component about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,24 +1,33 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white" >
-        <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full " >
-            <div className="pb-8">
-                <p className="text-4xl font-bold inline border-b-4 border-gray-500" >About</p>
-            </div>
-
-            <p className="text-xl mt-20">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id pariatur, fuga obcaecati, non neque nam fugit quas aut iure error molestiae rerum in quos, quae voluptas magnam asperiores eum expedita?
-            </p>
-            
-            <br/>
-
-            <p className="text-xl">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit blanditiis neque quisquam quia corrupti in, repudiandae reiciendis iusto perspiciatis eligendi? Quae adipisci atque, totam commodi quasi possimus quo laudantium deleniti.</p>
-
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full ">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
         </div>
-    </div>
-  )
-}
 
-export default About
+        <p className="text-xl mt-20">
+          Meu nome é Delano Quirino, tenho 29 anos, formado em Direito e nasci
+          em Teresina/PI. Trabalhei por 8 anos em clínicas e hospitais no setor
+          de digitação de laudos, onde era responsável pelo fluxo de emissão dos
+          laudos médicos. dev.
+        </p>
+
+        <br />
+
+        <p className="text-xl">
+          Atualmente, trabalho na minha empresa de produtos personalizados e paralelo a isso voltei a estudar no começo do ano de 2022 buscando me torna um Desenvolvedor Web e como podem ver estou correndo contra o tempo para aprender o máximo que posso para garantir uma vaga nesse mercado.Apesar de começar tarde, acredito que nesta área existam oportunidades para todos, até mesmo para aqueles que começaram um pouco tarde, mas que esta se esforçando e dedicando ao máximo.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
